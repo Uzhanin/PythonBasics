dfc863ce878e9d771d2fb0f7c4638e2f01802f15
Kirill Lopukhov <kloprnd@yandex.ru>
2020-01-23 02:24:30 +0300

    Начальный коммит

---
+++ Task1.py
@@ -0,0 +1,16 @@
+var_1 = 123
+var_2 = 3.14
+var_3 = 'Hello, world!'
+var_4 = False
+
+print(var_1)
+print(var_2)
+print(var_3)
+print(var_4)
+
+var_5 = input('Введите любую строку: ')
+var_6 = int(input('Введите целое число: '))
+
+print('Это пользовательская строка: ', var_5)
+print('Это пользовательское число: ', var_6)
+
