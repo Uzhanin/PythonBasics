9fe26d1bae2ee44e6c427508a56e5ddcb311965f
Kirill Lopukhov <kloprnd@yandex.ru>
2020-01-23 03:10:19 +0300

    Удалены все файлы

--- Task2.py
+++
@@ -1,9 +0,0 @@
-print('Переводим время в секундах в формат чч:мм:сс')
-user_time = int(input('Введите время в секундах: '))
-if user_time // 86400 > 0:
-    print('Да здесь же больше одних суток! Сутки при переводе не будут учтены')
-user_time = user_time % 86400
-hours = user_time // 3600
-min = (user_time % 3600) // 60
-sec = (user_time % 3600) % 60
-print(f'{user_time} секунд в формате чч:мм:сс это {hours:02}:{min:02}:{sec:02}')
--- Task3.py
+++
@@ -1,4 +0,0 @@
-n = input('Введите число n: ')
-sum = int(n) + int(n * 2) + int(n * 3)
-print(f'Ваше число {n}')
-print(f'Сумма вида {n} + {n}{n} + {n}{n}{n} равна {sum}')
